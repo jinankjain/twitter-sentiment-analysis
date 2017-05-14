e74ea5fee1deec0f4ae9b81e8359587c26cb4885
Alexandru Tifrea <alex.tifrea93@gmail.com>
2017-05-14 22:11:12 +0200

    Fixed bugs in utils

--- utils/make-data.sh
+++ utils/make-data.sh
@@ -8,6 +8,14 @@ function fail {
   exit 1
 }
 
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+   platform='freebsd'
+fi
+
 if ! [[ -d "${TWTR_DIR}" ]]; then
   fail "Twitter datasets not downloaded"
 fi
@@ -25,11 +33,15 @@ echo "organized dataset ready"
 
 if ! [[ -f "${DATA_DIR}/vocab.txt" ]]; then
   echo "Making vocabulary"
-  cat ../twitter-datasets/train_pos.txt ../twitter-datasets/train_neg.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c | sort -d -n -f1 -r | head -n 20000 > ../data/vocab.txt || fail "Could not create vocab"
+  if [[ ${platform} == "linux" ]]; then
+      cat ../twitter-datasets/train_pos.txt ../twitter-datasets/train_neg.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c | sort -d -n -f1 -r | head -n 20000 > ../data/vocab.txt || fail "Could not create vocab"
+  else
+      cat ../twitter-datasets/train_pos.txt ../twitter-datasets/train_neg.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c | sort -d -n -k1 -r | head -n 20000 > ../data/vocab.txt || fail "Could not create vocab"
+  fi
 fi
 echo "vocabulary ready"
 
-if ! [[ -f "${DATA_DIR}/vocab.txt" ]]; then
+if ! [[ -f "${DATA_DIR}/vocab_cut.txt" ]]; then
   echo "Trimming Vocabulary"
   cat ../data/vocab.txt | sed "s/^\s\+//g" | sed "s/\s\+/ /g" | sort -rn | grep -v "^[1234]\s" | cut -d' ' -f2 > ../data/vocab_cut.txt || fail "could not cut vocab"
 fi
@@ -37,7 +49,7 @@ echo "trimmed vocabulary ready"
 
 if ! [[ -f "${DATA_DIR}/vocab.pkl" ]]; then
   echo "Dumping data"
-  python3 pickle_vocab.py || fail "could not create pickle"
+  python3 pickle_vocab.py ${DATA_DIR} || fail "could not create pickle"
 fi
 
 echo "Make encodings of datasets"
--- utils/pickle_vocab.py
+++ utils/pickle_vocab.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 import pickle
+import sys
 
 
-def main():
+def main(root):
     vocab = dict()
-    with open('vocab_cut.txt') as f:
+    with open(root+'/vocab_cut.txt') as f:
         for idx, line in enumerate(f):
             vocab[line.strip()] = idx
 
-    with open('vocab.pkl', 'wb') as f:
+    with open(root+'/vocab.pkl', 'wb') as f:
         pickle.dump(vocab, f, pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == '__main__':
-    main()
+    root = sys.argv[1]
+    main(root)
