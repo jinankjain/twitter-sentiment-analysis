557268e902dcdc884fcb856f553a77532dbee5ab
Ubuntu <gpuvm@GpuVM.p5fkmrn5wkmeza55sx4xmpbnnc.ax.internal.cloudapp.net>
2017-06-17 20:07:48 +0000

    openai more changes

--- openai/dataSource.py
+++ openai/dataSource.py
@@ -1,15 +1,19 @@
 import numpy as np
+import random
 
 class DataSource:
-    def __init__(self, data_file):
+    def __init__(self, data_file, batch_size):
         self.start = 0
         self.dataset = {}
+        self.batch_size = batch_size
         with open(data_file, "r") as f:
             lines = f.readlines()
             split_lines = [line.strip("\n").split(' ', 1) for line in lines]
 
-            lines = [line[1] for line in split_lines]
-            targets = [int(line[0]) for line in split_lines]
+            lines = np.array([line[1] for line in split_lines])
+            targ =[line[0] for line in split_lines]
+            targets = [0 if t == '-1' else 1 for t in targ]
+            targets = np.array(targets)
 
             self.dataset["input"] = lines
             self.dataset["target"] = targets
@@ -33,7 +37,7 @@ class DataSource:
     #        batch_targets.append(target)
 
     #    return batch_inputs, batch_targets
-    def make_cvset(x, y, percent=0.1):
+    def make_cvset(self, x, y, percent=0.1):
         cross_validation_indices = np.array(random.sample(list(np.arange(len(y))), int(len(y) * percent) ))
         train_indices = np.array(list(set(np.arange(len(y))) - set(cross_validation_indices)))
 
@@ -43,19 +47,19 @@ class DataSource:
 
 
 
-    def train_bitches(self, batch_size):
+    def train_bitches(self):
         batch_inputs = []
         batch_targets = []
-        end = self.start + batch_size
+        end = self.start + self.batch_size
         batch_inputs = self.dataset["input"][self.start:end]
         batch_targets = self.dataset["target"][self.start:end]
 
         self.start = end
 
-        if (len(batch_inputs) < batch_size):
-            rest = batch_size - len(batch_inputs)
-            batch_inputs += self.dataset["input"][:rest]
-            batch_targets += self.dataset["target"][:rest]
+        if (len(batch_inputs) < self.batch_size):
+            rest = self.batch_size - len(batch_inputs)
+            batch_inputs = np.append(batch_inputs, self.dataset["input"][:rest])
+            batch_targets = np.append(batch_targets, self.dataset["target"][:rest])
             self.start = rest
 
-        yield make_cvset(batch_inputs, batch_targets, 0.1)
+        return batch_inputs, batch_targets
--- openai/train_openai.py
+++ openai/train_openai.py
@@ -1,10 +1,10 @@
-
 import numpy as np
 import random
 from keras.models import Sequential
-from keras.layers import Dense,Activation
+from keras.layers import Dense, Dropout, Activation
 from keras.optimizers import Adam
 from keras.utils.np_utils import to_categorical
+from keras import backend as K
 import os
 from keras.models import model_from_json
 import sys
@@ -14,8 +14,12 @@ from encoder import Model
 #x=np.load('X.npy')
 #y=np.load('Y.npy' )
 #y = to_categorical(y)
+encoder = Model()
 
-def baseline_model():
+def log(*s):
+    print("TRAIN_OPENAI:", s)
+
+def old_baseline_model():
 	# create model
     model = Sequential()
     model.add(Dense(4096, input_shape=(4096,), init='lecun_uniform'))
@@ -25,14 +29,24 @@ def baseline_model():
 
     return model
 
+def baseline_model():
+	# create model
+    model = Sequential()
+    model.add(Dense(4096, input_shape=(4096,), init='glorot_normal', activation='relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(2000, init='glorot_normal', activation='relu'))
+    model.add(Dropout(0.25))
+    model.add(Dense(2, init='lecun_uniform'))
+    model.add(Activation('softmax'))
 
-def train():
-    encoder = Model()
+    return model
+
+def train(num_epochs):
     if not os.path.exists("model.h5"):
-        print("trained model not found, making new model")
+        log("trained model not found, making new model")
         bm=baseline_model()
     else:
-        print("trained model found, loading it...")
+        log("trained model found, loading it...")
         json_file = open('model.json', 'r')
         loaded_model_json = json_file.read()
         json_file.close()
@@ -43,47 +57,68 @@ def train():
                   optimizer=Adam(),
                   metrics=['accuracy'])
 
-    datasource = DataSource("../data/small_train.txt")
+    datasource = DataSource("../data/small_train.txt", 20000)
 
-    oi = 0
-    for x_train, y_train, x_test, y_test in train_bitches(20000):
-        print("OpenAI Batch ", oi)
-        oi += 1
-        x_train = encoder.transform(x_train)
-        x_test = encoder.transform(x_test)
-        y_train = to_categorical(y_train)
-        y_test = to_categorical(y_test)
-        bm.fit(x_train, y_train,
-                        batch_size=256, nb_epoch=1,
-                        verbose=1, validation_data=(x_test, y_test), shuffle=False)
-    model_json = bm.to_json()
-    with open("model.json", "w") as json_file:
-        json_file.write(model_json)
-    # serialize weights to HDF5
-    bm.save_weights("model.h5")
-    print("Saved model to disk")
+    with K.get_session():
+        for oi in range(num_epochs):
+            log("OpenAI Batch ", oi)
+            x, y = datasource.train_bitches()
+            x = encoder.transform(x)
+            y = to_categorical(y, 2)
+            x_train, y_train, x_test, y_test = datasource.make_cvset(x, y, 0.1)
+            bm.fit(x_train, y_train, batch_size=256, epochs=1, verbose=1, 
+                    validation_data=(x_test, y_test), shuffle=False)
+            if (oi + 1) % 5 == 0:
+                log("saving model")
+                model_json = bm.to_json()
+                with open("output/" + str(oi) + ".json", "w") as json_file:
+                    json_file.write(model_json)
+                # serialize weights to HDF5
+                bm.save_weights("output/" + str(oi) + ".h5")
+        make_submission(bm)
+        model_json = bm.to_json()
+        with open("output/final.json", "w") as json_file:
+            json_file.write(model_json)
+        # serialize weights to HDF5
+        bm.save_weights("output/final.h5")
+        K.clear_session()
+        log("Saved model to disk")
 
-    score = bm.evaluate(x_test, y_test, verbose=1)
-    print('Test score:', score[0])
-    print('Test accuracy:', score[1])
+def make_submission(model):
+    submit_examples = list(open("../twitter-datasets/test_data.txt", "r").readlines())
+    submit_examples = [s.strip() for s in submit_examples]  
+    splitter = [s.split(',', 1) for s in submit_examples]
+    sentences = np.array([s[1] for s in splitter])
+    ids = [s[0] for s in splitter]
+    x = encoder.transform(sentences)
+    result = np.argmax(model.predict(x, batch_size=256, verbose=1), axis=1)
+    result = ['1' if r == 1 else '-1' for r in result]
+    log('saving submissions')
+    with open("../twitter-datasets/openaisubmission.csv", "w") as f:
+        f.write("Id,Prediction\n")
+        for i,p in enumerate(result):
+            f.write(str(ids[i]) + "," + str(p) + "\n")
 
-def evaluate():
-    print("loading model...")
-    json_file = open('model.json', 'r')
+def load_and_submit(id='final'):
+    log("loading model...")
+    json_file = open('output/' + id + '.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
-    loaded_model.load_weights("model.h5")
+    loaded_model.load_weights("output/" + id + ".h5")
     loaded_model.compile(loss='categorical_crossentropy',
                   optimizer=Adam(),
                   metrics=['accuracy'])
-    print("Loaded model from disk")
-    score = loaded_model.evaluate(x_test, y_test, verbose=1)
-    print('Test score:', score[0])
-    print('Test accuracy:', score[1])
+    log("Loaded model from disk")
+    make_submission(loaded_model)
+    #score = loaded_model.evaluate(x_test, y_test, verbose=1)
+    #log('Test score:', score[0])
+    #log('Test accuracy:', score[1])
 
 if __name__ == "__main__":
     if sys.argv[1] == "train":
-        train()
+        train(int(sys.argv[2]))
+    elif sys.argv[1] == "submit":
+        load_and_submit(sys.argv[2])
     else:
-        evaluate()
+        raise NotImplementedError
