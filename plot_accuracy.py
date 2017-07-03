d403ed2665765d37a712e66012973bcb5d6cc611
Jinank Jain <jinank94@gmail.com>
2017-07-03 09:42:28 -0400

    Plot training and validation accuracy

---
+++ plot_accuracy.py
@@ -0,0 +1,39 @@
+import os
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+ROOT_DIR = "training_stats/*.stats"
+files = glob.glob(ROOT_DIR)
+
+labels = ['Bidirectional LSTM', 'CNN + LSTM', 'GRU', 'Swiss Cheese', 'Third Seq CNN']
+
+def plot_graph(type:str):
+    fig, ax = plt.subplots()
+    if type == "training":
+        draw_accuracy(ax, type)
+        label_y = ax.set_ylabel('Training accuracy', fontsize = 15)
+    elif type == "validation":
+        draw_accuracy(ax, type)
+        label_y = ax.set_ylabel('Validation accuracy', fontsize = 15)
+    legend = ax.legend(loc=4, shadow=True)
+    plt.grid(True)
+    ax = plt.gca()
+    ax.xaxis.set_ticks(np.arange(0,120, 10))
+    label_x = ax.set_xlabel('Number of batches', fontsize = 15)
+    plt.show()
+
+def draw_accuracy(ax, type):
+    i = 0
+    for f in files:
+        if type == "training":
+            reader = np.loadtxt(f, dtype=float, delimiter=',', skiprows=1, usecols=(1,))
+        elif type == "validation":
+            reader = np.loadtxt(f, dtype=float, delimiter=',', skiprows=1, usecols=(3,)) 
+        x = range(1, len(reader)+1)
+        ax.plot(x, reader, label=labels[i])
+        i+=1
+
+if __name__ == "__main__":
+    plot_graph(sys.argv[1])
