6ef28eb62e6468e8ef172ddb8834d5357eca1336
Jinank Jain <jinank94@gmail.com>
2017-07-03 10:33:07 -0400

    Updated majority vote to run all the models

--- majority_vote.py
+++ majority_vote.py
@@ -1,18 +1,33 @@
 import os
 
+RUN_SEQ_CONV = "python3 vanilla_lstm_model.py --is_eval --emb_len=400" \
+               " --embedding_type=word2vec" \
+               " --ckpt_file=data/checkpoints/seq_conv_lstm_adam_ckpt-10000000-0.27.hdf5" \
+               " --seq_conv --train_file=data/full_train.txt"
+
+RUN_BIDI = "python3 vanilla_lstm_model.py --is_eval --emb_len=200" \
+           " --embedding_type=glove" \
+           " --ckpt_file=data/checkpoints/bidi_lstm_adam_ckpt-15600000-0.26.hdf5" \
+           " --bidi --train_file=data/full_train.txt"
+
+RUN_GRU = "python3 vanilla_lstm_model.py --is_eval --emb_len=200" \
+          " --embedding_type=glove" \
+          " --ckpt_file=data/checkpoints/gru_lstm_adam_ckpt-19200000-0.26.hdf5" \
+          " --gru --train_file=data/full_train.txt"
+
+RUN_SWISS_CHESSE = "python3 vanilla_lstm_model.py --is_eval --emb_len=400" \
+                   " --embedding_type=word2vec" \
+                   " --ckpt_file=data/checkpoints/swisscheese_lstm_adam_ckpt-11400000-0.28.hdf5" \
+                   " --swisscheese --train_file=data/full_train.txt"
+
+os.system(RUN_SEQ_CONV)
+os.system(RUN_BIDI)
+os.system(RUN_GRU)
+os.system(RUN_SWISS_CHESSE)
+
 SEQ_LEN = 40
 ROOT_DIR = "data"
-TEST_FILES = ["lstm.txt", "openai.csv", "cnn.csv", "multi.txt", "bidi.txt"]
-
-
-data_source = DataSource(
-    vocab=vocab,
-    labeled_data_file="data/full_data.txt",
-    test_data_file="data/test_data.txt",
-    embedding_file=None,
-    embedding_dim=None,
-    seq_length=SEQ_LEN,
-    embedding_type=None)
+TEST_FILES = ["swisscheese_test_output.txt", "gru_test_output.txt", "seq_conv_test_output.csv", "bidi_test_output.txt"]
 
 result = []
 
